6a426dc8662c94be843e96c34f5172328b3c5a56
Nguyen Truc Hung <hung_nt@care-connect.vn>
2021-09-16 16:02:21 +0900

    build structure

---
+++ Models/VoteItem/VoteItem.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace NetCoreReactJS.Models.VoteItem
+{
+    public class VoteItem
+    {
+        public int Id { get; set; }
+        public string Name { get; set; }
+        public string VoteContent { get; set; }
+        public DateTime Sdate { get; set; }
+        public DateTime Edate { get; set; }
+    }
+}
